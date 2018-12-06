7298771d9da1c1213c41a951053ad00db0a2357f
Drakandes <albrano8@hotmail.com>
2018-12-06 17:07:46 -0500

    Minor addition

--- my-app/src/app/path-exploration.service.ts
+++ my-app/src/app/path-exploration.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import  explorationData from '../json-stuff/explorationAreas'
+import  explorationDataObject from '../json-stuff/explorationAreas'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PathExplorationService {
+
+  public explorationData = explorationDataObject
 
   constructor() { }
 
